dfe570484473cf178973fcefd189b93d0a00b88a
evsey9 <evseyru1@mail.ru>
2020-04-23 10:27:17 +0500

    поправки в коде

--- ONTI_2020_RobotMasters.ino
+++ ONTI_2020_RobotMasters.ino
@@ -1,37 +1,42 @@
+#include <HCSR04.h>
 #include <TroykaMQ.h>
-#include <iarduino_HC_SR04.h>
+
 
 
 // Пины и константы
 
+#define echoPin 11
+#define triggerPin 10
 #define PIN_MQ135 A0                 // Датчик газа
 #define dCO2 20                      // Максимальное значение отклонения уровня газа
-#define pinWDat 12                   // Датчик перелива
-#define pumpdir1 4                   // Первая помпа направление (пин моторшилда H1)
-#define pumpspd1 5                   // Первая помпа скорость (пин моторшилда E1)
-#define pumpdir2 7                   // Вторая помпа направление (пин моторшилда H2)
-#define pumpspd2 6                   // Вторая помпа скорость (пин моторшилда E1)
+#define pinWaterSensor 12                   // Датчик перелива
+#define pumpDirPin1 4                   // Первая помпа направление (пин моторшилда H1)
+#define pumpSpeedPin1 5                   // Первая помпа скорость (пин моторшилда E1)
+#define pumpDirPin2 7                   // Вторая помпа направление (пин моторшилда H2)
+#define pumpSpeedPin2 6                   // Вторая помпа скорость (пин моторшилда E1)
 
 // Переменные
-uint8_t pumpspd = 0;                 // Скорости помп
-bool    pumpdir = HIGH;              // Направления помп
-int     CO2     = 0;                 // Среднее значение уровня газа
-
+uint8_t pumpSpeed = 0;                 // Скорости помп
+bool    pumpDir = HIGH;              // Направления помп
+int     CO2Opt     = 0;// Среднее значение уровня газа
+int     CO2 = 0;
 MQ135 mq135(PIN_MQ135);
+UltraSonicDistanceSensor distSensor(triggerPin, echoPin);
 
 void setup(){
   Serial.begin(9600);
 
-  pinMode(pumpdir1, OUTPUT); digitalWrite(pumpdir1, pumpdir);          // Конфигурируем вывод pinH1 как выход и устанавливаем на нём уровень логического «0»
-  pinMode(pumpdir2, OUTPUT); digitalWrite(pumpdir2, pumpdir);          // Конфигурируем вывод pinE1 как выход и устанавливаем на нём уровень логического «0»
-  pinMode(pumpspd1, OUTPUT); digitalWrite(pumpspd1, 0);
-  pinmode(pumpspd2, OUTPUT); digitalWrite(pumpspd2, 0);
-  pinMode(pinWDat, INPUT);
+  pinMode(pumpDirPin1, OUTPUT);
+  digitalWrite(pumpDirPin1, pumpDir);
+  pinMode(pumpDirPin2, OUTPUT); digitalWrite(pumpDirPin2, pumpDir);
+  pinMode(pumpSpeedPin1, OUTPUT); changeOutPumpState(false); // Выключаем в самом начале помпу в жилом боксе
+  pinMode(pumpSpeedPin2, OUTPUT); changeInPumpState(false); // Выключаем в самом начале помпу в боксе с водой
+  pinMode(pinWaterSensor, INPUT);
   delay(60000);
   mq135.calibrate();
   Serial.print("CO2(0) = ");
   Serial.println(mq135.readCO2());
-  colibration();
+  gasCalibration();
 }
 
 void loop() {
@@ -39,15 +44,25 @@ void loop() {
 
 }
 
-void colibration(){ // Калибровка
-  digitalWrite(pumpspd1, HIGH); // Начинаем заливать воду
-  while(digitalRead(pinWDat)==0){
+void changeInPumpState(bool state) { // Включает/выключает насос в боксе с водой
+  digitalWrite(pumpSpeedPin2, state);
+}
+
+void changeOutPumpState(bool state) { // Включавет/выключает насос в жилом боксе
+  digitalWrite(pumpSpeedPin1, state);
+}
+
+void gasCalibration(){ // Калибровка
+  changeInPumpState(true); // Начинаем заливать воду
+  while(digitalRead(pinWaterSensor) == 0){
     delay(1);
   }
-  digitalWrite(pumpspd1, LOW);
+  changeInPumpState(false);
+  CO2Opt = mq135.readCO2();
   delay(10000);
-  CO2=mq135.readCO2()/2;
-  Serial.print("CO2(const) = ");
-  Serial.println(CO2);
+  CO2Opt += mq135.readCO2();
+  CO2Opt = CO2Opt / 2;
+  Serial.print("Optimal Gas Level(const) = ");
+  Serial.println(CO2Opt);
   return ;
 }
